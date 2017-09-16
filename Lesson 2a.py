18155405252d479d7e7e5bccdb6dcb73d1727c80
190849 <31870983+190849@users.noreply.github.com>
2017-09-16 22:34:10 +0800

    Add files via upload

---
+++ Lesson 2a.py
@@ -0,0 +1,35 @@
+print("\"They call me big Mike\" " * 10)
+print("""
+
+──────▄▄▄▄▄███████████████████▄▄▄▄▄──────
+────▄██████████▀▀▀▀▀▀▀▀▀▀██████▀████▄────
+──▄██▀████████▄─────────────▀▀████─▀██▄──
+─▀██▄▄██████████████████▄▄▄─────────▄██▀─
+───▀█████████████████████████▄────▄██▀───
+─────▀████▀▀▀▀▀▀▀▀▀▀▀▀█████████▄▄██▀─────
+───────▀███▄──────────────▀██████▀───────
+─────────▀██████▄─────────▄████▀─────────
+────────────▀█████▄▄▄▄▄▄▄███▀────────────
+──────────────▀████▀▀▀████▀──────────────
+────────────────▀███▄███▀────────────────
+───────────────────▀█▀───────────────────
+
+""")
+
+v_month = "October"
+print(v_month)
+print(str.capitalize(v_month))
+
+"name = input("What's your name? ")
+print("Nice to meet you " + name + "!")
+print("Where will you be travelling today " + name + "?" "America or Philippines ")
+
+
+age = int(input("Your age? "))
+print("So, you are already " + str(age) + " years old, " + name + "!")
+print("you have been alive for",  int(age) * 365, "days! Thats pretty cool")
+days = int(age) * 365
+print("Its pretty cool that you have been alive for", int(days) * 24, "hours!! That's a really long time!")
+hours = int(days) * 24
+print("In case you were wondering, you have been alive for", int(hours) * (60*60), "seconds. Woah!")
+"
